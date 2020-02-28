31ee9662b2cdca2dd30dfc62ce52a3c157d3f712
haisenbao <2286146585@qq.com>
2020-02-29 05:27:03 +0800

    Initial commit

---
+++ lock/src/test/java/top/kispower/skill/lock/LockApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kispower.skill.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
