a1ed4e9950b90ee2bac85d0fee89ee8782ae8555
haisenbao <2286146585@qq.com>
2020-05-14 22:54:47 +0800

    delete skill/skill  2

---
+++ websocket/src/main/java/top/kispower/skill/websocket/WebsocketApplication.java
@@ -0,0 +1,13 @@
+package top.kispower.skill.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketApplication.class, args);
+    }
+
+}
---
+++ websocket/src/test/java/top/kispower/skill/websocket/WebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kispower.skill.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
