44963892191a04922c305e6d129fee1cc2409ebf
haisenbao <2286146585@qq.com>
2020-06-06 22:21:02 +0800

    delete skill/skill  2

---
+++ flink/src/main/java/top/kispower/skill/flink/FlinkApplication.java
@@ -0,0 +1,13 @@
+package top.kispower.skill.flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkApplication.class, args);
+    }
+
+}
---
+++ flink/src/test/java/top/kispower/skill/flink/FlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package top.kispower.skill.flink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
